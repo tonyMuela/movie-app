21847c15b14d183298b437feb63c559d02e1b4bd
Tony Muela <166438176+tonyMuela@users.noreply.github.com>
2024-09-30 23:28:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${{ secrets.API_KEY }}&page=1";
+const APIKEY = ${{ secrets.API_KEY }};
+const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=${{ secrets.API_KEY }}&query=";              
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}&query=";              
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
